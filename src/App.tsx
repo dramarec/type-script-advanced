2e8653717f53f891882392bcc1254ac1a5422665
Oleg Dramaretskyi <dramarec@gmail.com>
2021-05-21 08:48:59 +0300

    added start proj

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// FC принемает только фунциональные компоненты
+import React, { FC } from 'react';
+import Header from './components/Header';
+
+const App: FC = () => {
+    return <Header />;
+};
+
+export default App;
